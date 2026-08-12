cad1ddc591e40472ccd93ec704aade48684431e4
E.D. <edwnr@proton.me>
2026-08-11 22:49:12 -0600

    Updated main.py
    Slight error not putting the hashing file in encryption.py

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 import os
 import hashlib
 from startup import *
-
-def sha256_file(path: str, chunk_size: int = 1024 * 1024) -> str:
-    h = hashlib.sha256()
-    with open(path, "rb") as f:
-        for chunk in iter(lambda: f.read(chunk_size), b""):
-            h.update(chunk)
-    return h.hexdigest()
+from encryption import *
 
 word_list = "wordlist.csv"
 word_list_hash = sha256_file(word_list)
@@ -15,7 +9,6 @@ password_database = "passwords.csv"
 
 def main():
     startup_detecting_all_files()
-
-
+    
 if __name__ == "__main__":
     main()
